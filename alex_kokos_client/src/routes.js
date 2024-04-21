2af85958bcecb6e7ce38acb4f1ee367362ae0caf
Lexa <aadamovich2003@gmail.com>
2024-04-22 00:35:47 +0300

    start setings for react clinet...

---
+++ alex_kokos_client/src/routes.js
@@ -0,0 +1,13 @@
+import Admin from './pages/Admin'
+import { ADMIN_ROUTE } from './utils/consts'
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Commponent: Admin
+    }
+]
+
+export const publicRoutes = [
+
+]
